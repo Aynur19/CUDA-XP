4c2a9f8bccb6966aff04e6c57326de96f69484ac
Aynur19 <nasybullin.aynur@yandex.ru>
2021-11-09 21:01:17 +0300

    creation of a project for passing the book "CUDA Technology in Examples"

---
+++ Book_CudaTechnologyInExamples_2013/Chapter3_Introduction/chapter3.h
@@ -0,0 +1,9 @@
+#ifndef __CHAPTER3_INTRODUCTION_H__
+#define __CHAPTER3_INTRODUCTION_H__
+
+#include "../../_common/helper.h"
+#include "../../_common/helper.cuh"
+
+
+
+#endif // !__CHAPTER3_INTRODUCTION_H__
---
+++ Book_CudaTechnologyInExamples_2013/Chapter3_Introduction/main.cpp
@@ -0,0 +1,5 @@
+#include "chapter3.h"
+
+int main(int argc, char* argv) {
+	getGpuInfo();
+}
